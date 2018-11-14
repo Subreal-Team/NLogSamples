e26929a1d729f4944ce9add451decd33b30b505d
Aleksey Timofeev <alex.v.timofeev@gmail.com>
2018-11-14 21:42:53 +0500

    initial commit

---
+++ demoNlog/Program.cs
@@ -0,0 +1,19 @@
+﻿using NLog;
+
+namespace demoNlog
+{
+    internal class Program
+    {
+        private static Logger logger = LogManager.GetCurrentClassLogger();
+
+        private static void Main(string[] args)
+        {
+            logger.Trace("trace message");
+            logger.Debug("debug message");
+            logger.Info("info message");
+            logger.Warn("warn message");
+            logger.Error("error message");
+            logger.Fatal("fatal message");
+        }
+    }
+}
